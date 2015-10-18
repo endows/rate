91c953200f1e503af36673ac5d276ad9072921ae
takashi <endotakashi1992@gmail.com>
2015-10-18 11:59:24 +0900

    end

--- convert.js
+++ convert.js
@@ -1,11 +1,19 @@
 var fs = require('fs');
+var ss = require('simple-statistics');
+
 fs.readFile('./data.json', 'utf8', function (err, text) {
 
     obj = JSON.parse(text)
-    console.log(obj)
     ar = []
     Object.keys(obj).forEach(function (key) {
       ar.push(obj[key])
     });
-    console.log(ar)
+
+    mean = ss.mean(ar)
+    sd = ss.sampleStandardDeviation(ar)
+
+    Deviation = (1500 - mean) / sd * 10 + 50
+    console.log('deviation is:',Deviation)
+    // console.log(ss.mean(ar))
+    // console.log(ss.sampleStandardDeviation(ar))
 });
